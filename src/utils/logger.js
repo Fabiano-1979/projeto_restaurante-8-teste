52c7d14fde791bd8eb584fb74547981c70b6e18c
Fabiano-@79 <61099842+Fabiano-1979@users.noreply.github.com>
2026-06-05 11:19:31 -0300

    feat: add logger utility (Pino)

---
+++ src/utils/logger.js
@@ -0,0 +1,15 @@
+const pino = require('pino');
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname',
+    },
+  },
+});
+
+module.exports = logger;
